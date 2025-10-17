839f1ea8939633ef3230a6ba9838209120cabd9c
Kkamiyan <carlohilo0000@gmail.com>
2025-10-17 11:47:25 +0800

    groundwork

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.nio.file.*;
+import java.io.*;
 import javax.crypto.*;
 
 
@@ -11,7 +12,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        encrypt(getFile());
+        //idSystem();
+        encrypt(getFile(), idSystem());
 
 
         /*System.out.println("Data Conceal System");
@@ -35,17 +37,19 @@ public class Main {
 
     }
     
-    static void encrypt(Path filePath){
+    static void encrypt(Path filePath,int idSystem){
         
-        String encryptedFilesPath = "Data-Conceal-System\\Encrypted-Files";
+        String encryptedFilesPath = idSystem + "test.txt";
+        System.out.println("filename: " + encryptedFilesPath);
         Path file = Paths.get(encryptedFilesPath);
 
-        if(!Files.exists(file)){
-            System.out.println("DOEsnt exist");
-        }
+
 
         System.out.println("Encrypting file...");
         try{
+            FileWriter writer = new FileWriter(idSystem + "test.txt", true);
+
+
             System.out.println("1");
             byte[] fileData = Files.readAllBytes(filePath);
             System.out.println("2");
@@ -53,13 +57,18 @@ public class Main {
             System.out.println("3");
             genKey.init(128);
             SecretKey key = genKey.generateKey();
-
+            System.out.println("4");
             Cipher encrypt = Cipher.getInstance("AES");
             encrypt.init(Cipher.ENCRYPT_MODE, key);
-
+            System.out.println("5");
             byte[] encryptedFile = encrypt.doFinal(fileData);
+            System.out.println("6");
+
+
+
 
             Files.write(file, encryptedFile);
+            System.out.println("7");
             System.out.println("File encrypted successfully!");
         } catch (Exception e) {
             System.out.println("Something went wrong...");
@@ -72,15 +81,55 @@ public class Main {
     }
 
     static Path getFile(){
-        String filePath = "";
+        String filePath = "README.md";
         System.out.print("Enter the file path: ");
-        //filePath = scanner.nextLine();
+        filePath = scanner.nextLine();
         Path file = Paths.get(filePath);
         
         return file;
     }
 
+    static int idSystem(){
+        String line;
+        int count = 0;
 
+        try {
+            String idFilePath = "id.txt";
+            
+
+            FileReader reader = new FileReader(idFilePath);
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.printf("ID: %06d\n", count);
+                count++;
+
+            }
+            
+            bufferedReader.close();
+
+            FileWriter write = new FileWriter(idFilePath, true);
+            write.write(count + "\n");
+            write.close();
+
+        } catch (Exception e) {
+            System.out.println("Something went wrong...");
+        }
+        return count + 1;
+    }
+
+    static void test(){
+        try {
+            Path file = Paths.get("README.md");
+            if(Files.exists(file)){
+                System.out.println("exist");
+            }
+            else System.out.println("does not exist");
+            
+        } catch (Exception e) {
+            System.out.println("error");
+        }
+    }
 
 
 }
